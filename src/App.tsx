2a94f1ca4c27e49f2e683b40324854a771355df9
Simo-C3 <shimomae.hitoshi210@mail.kyutech.jp>
2023-12-17 02:08:09 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import './firebaseApp'
+
+import { useEffect, useState } from 'react'
+import {
+  getAuth,
+  Auth,
+  GithubAuthProvider,
+  signInWithPopup,
+} from 'firebase/auth'
 
 function App() {
+  const [token, setToken] = useState<string | null>(null)
+  const [auth, setAuth] = useState<Auth | null>(null)
+  const [provider, setProvider] = useState<GithubAuthProvider | null>(null)
+
+  // GitHub OAuth Provider ObjectのInstanceを作成
+  useEffect(() => {
+    if (provider === null) {
+      const newProvider = new GithubAuthProvider()
+      newProvider.addScope('repo') // 既定ではユーザー自身のemailを取得するスコープしか付与されない。必要に応じてスコープを追加する
+      setProvider(newProvider)
+    }
+  }, [provider])
+
+  // Firebase Appに対するAuth instanceを取得
+  useEffect(() => {
+    if (provider !== null && auth === null) {
+      setAuth(getAuth())
+      console.log(auth)
+    }
+  }, [auth, provider])
+
+  // ポップアップによるサインインを実施し、成功したらアクセストークンを取得する
+  useEffect(() => {
+    if (provider !== null && auth !== null && token === null) {
+      signInWithPopup(auth, provider).then((result) => {
+        const credential = GithubAuthProvider.credentialFromResult(result)
+        if (credential && credential.accessToken) {
+          setToken(credential.accessToken)
+          console.log('token: ' + credential.accessToken)
+        }
+        console.log(result.user)
+      })
+    }
+  }, [auth, provider, token])
+
+  // // アクセストークンを使用してGitHub API（GET /Issues）へリクエストする
+  // useEffect(() => {
+  //   if (token !== null) {
+  //     fetch(`https://api.github.com/repos/${OWNER}/${REPO}/issues`, {
+  //       headers: {
+  //         Authorization: `token ${token}`,
+  //         Accept: 'application / vnd.github.v3 + json',
+  //       },
+  //     }).then((result) => {
+  //       result.json().then((result) => {
+  //         console.log(result)
+  //       })
+  //     })
+  //   }
+  // }, [token])
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,9 +77,10 @@ function App() {
         >
           Learn React
         </a>
+        <div>{token !== null && <p>token: {token}</p>}</div>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/firebaseApp.tsx
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCYvmoNfkPgWke9n45DtHilab4J-fqKxhQ',
+  authDomain: 'kiikuten.firebaseapp.com',
+  projectId: 'kiikuten',
+  storageBucket: 'kiikuten.appspot.com',
+  messagingSenderId: '1064654686614',
+  appId: '1:1064654686614:web:fc6eb86e90daa58c62f5b7',
+}
+
+initializeApp(firebaseConfig)
